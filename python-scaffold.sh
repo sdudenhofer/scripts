a49064c18a24d6d4a0b2b383f4dced908b3ebd33
Seth Dudenhofer <sdudenhofer@gmail.com>
2024-10-29 14:31:31 -0700

    added logic to init uv via script

--- python-scaffold.sh
+++ python-scaffold.sh
@@ -8,3 +8,17 @@ echo "Create data directory"
 
 mkdir data/raw
 echo "Create location for raw data"
+
+while true; do
+  read -p "Initialize UV?" yesno
+  case $yesno in
+  [Yy]*)
+    echo "Initializing uv..."
+    uv init
+    ;;
+  [Nn]*)
+    echo "skipping uv initialization..."
+    ;;
+  *) echo "Must answer either yes or no" ;;
+  esac
+done

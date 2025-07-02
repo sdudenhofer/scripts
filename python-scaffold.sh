9e89d2a49b8a6ff94514c83793524981a5febc40
Seth Dudenhofer <sdudenhofer@gmail.com>
2025-07-02 13:01:04 -0500

    updated script for new folder structure

--- python-scaffold.sh
+++ python-scaffold.sh
@@ -1,13 +1,26 @@
 #! /bin/bash
 
-mkdir src/
+mkdir app/
 echo "Create src directory"
 
-mkdir data/
-echo "Create data directory"
+mkdir app/core/
+echo "Created core folder"
 
-mkdir data/raw
-echo "Create location for raw data"
+mkdir app/api/v1
+echo "Created location for api version 1"
+
+mkdir app/db/
+echo "Created db data location"
+
+mkdir app/schemas/
+echo "Created schema location"
+
+mkdir app/services
+echo "Create services location"
+
+mkdir app/tests
+echo "Base test folder created"
+
 
 while true; do
   read -p "Initialize UV?" yesno
